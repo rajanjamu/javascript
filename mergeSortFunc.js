23da5779e2b18a647a54e3fe8291f1f85d005386
Rajan Gupta <rajanjamu@gmail.com>
2014-12-05 15:43:29 +0530

    added javascript file to mergesort.html

--- mergeSortFunc.js
+++ mergeSortFunc.js
@@ -1,52 +1,47 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Merge Sort</title>
-	<link rel="stylesheet" type="text/css" href="main.css">
-	<script language="javascript" src="mergeSortFunc.js"></script>
-</head>
-<body>
+// SORTING ARRAY USING MERGE-SORT ALGORITHM
+// AUTHOR: RAJAN GUPTA
+// CREATED: 1 DEC 2014
+// UPDATED: 1 DEC 2014
 
-<h2>Merge Sort Algorithm</h2>
-
-<pre>
-Author: Rajan Gupta
-Created: Fri Dec 05 2014 11:47:34 GMT+0530 (IST)
-Visited: <span id="date"></span>
-</pre>
-
-<h3>Input</h3>
-<pre id="input"></pre>
-
-<h3>Code</h3>
-<pre>
+// Merge-Sort function
 function merge_sort(inArray) {
 	if (inArray.length > 1) {
+		// Split array using split function
 		var splitArray = split(inArray);
+		// Returns sorted array if array.len > 1
 		return merge(merge_sort(splitArray[0]), merge_sort(splitArray[1]));
 	}
 	else {
+		// Returns array if array.len == 1
 		return inArray;
 	}
 }
 
+// Split the input array inArray into two equal/almost-equal parts
+// Returns two split arrays within master array
 function split(inArray) {
 	var outArray = [];
 	var len = inArray.length;
 	if (len % 2 == 0) {
+		// Equally split the array into two parts
 		return outArray = [inArray.slice(0,len/2),inArray.slice(len/2,len)];
 	}
 	else {
+		// Unequally split with first array having one extra element than second array
 		return outArray = [inArray.slice(0,len/2+1),inArray.slice(len/2+1,len+1)];
 	}
 }
 
+// Merges two input arrays in ascending order
 function merge(inArray1, inArray2) {
 	var len1 = inArray1.length;
 	var len2 = inArray2.length;
 	var i = 0, j = 0, k = 0;
 	var outArray = []
+	// Run the loop till program reaches end of any of the array
 	while (i < len1 && j < len2) {
+		// Add element to outArray according to which one is higher
+		// Keep incrementing the index of the array whose element has been added
 		if (inArray1[i] < inArray2[j]) {
 			outArray[k] = inArray1[i]
 			i += 1
@@ -57,21 +52,8 @@ function merge(inArray1, inArray2) {
 		}
 		k += 1
 	}
+	// Concatenate the remaining elements of the array index of which has not reached the end
 	if (i == len1) {outArray = outArray.concat(inArray2.slice(j,len2+1));}
 	if (j == len2) {outArray = outArray.concat(inArray1.slice(i,len1+1));}
 	return outArray;
 }
-</pre>
-
-<h3>Answer</h3>
-<button class="button" onclick="document.getElementById('answer').innerHTML = merge_sort(array)">Click me to see the answer</button>
-<pre id="answer"></pre>
-
-<script>
-array = [10,99,5,3,7,9,6,3];
-document.getElementById("input").innerHTML = array
-document.getElementById("date").innerHTML = Date()
-</script>
-
-</body>
-</html> 
